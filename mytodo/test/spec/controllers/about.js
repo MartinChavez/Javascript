e2ad1790dc237845a3be43f4aa810ae27018ac19
MartinChavez <martin.chavez@live.com>
2015-04-17 19:57:31 -0700

    Added test Unit Test

--- mytodo/test/spec/controllers/about.js
+++ mytodo/test/spec/controllers/about.js
@@ -19,4 +19,9 @@ describe('Controller: AboutCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+
+  it('basic math', function () {
+    expect(3).toBe(3);
+  });
+
 });
