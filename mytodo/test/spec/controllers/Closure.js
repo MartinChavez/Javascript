0af7355513633e2fd3eb03cefefcbb9dad74a420
MartinChavez <martin.chavez@live.com>
2015-05-09 08:57:16 -0700

    cLOSURE UPDATE

--- mytodo/test/spec/controllers/Closure.js
+++ mytodo/test/spec/controllers/Closure.js
@@ -35,5 +35,47 @@ describe('Controller: MainCtrl', function () {
 
     expect(insideClosure).toBe("Closure Variable");
   });
+  it('Closure functions can modify bound variables in the background', function () {
+
+    function outsideClosure() {
+
+      var numberOfTimes = 0;
+
+      function insideClosure() {
+        numberOfTimes++;
+        return numberOfTimes;
+      }
+
+      return insideClosure;
+    }
+
+    var insideClosureOne = outsideClosure();
+    insideClosureOne(); //Calling it one time increments numberOfTimes to 1
+    insideClosureOne(); //Calling it two times increments numberOfTimes to 2
+    //Calling it the third times increments numberOfTimes to 3
+    expect(insideClosureOne()).toBe(3);
+  });
+  it('Closures bind values at the very last opportunity ', function () {
+
+    function outsideClosure() {
+
+      var key = 3;
+      var insideFunction;
+
+      for (var currentNumber = 0; currentNumber < 6; currentNumber++) {
+
+        if (currentNumber === key) {//Although we match the key 3, the loop continues and the function will always return the last value
+          insideFunction = function insideClosure() {
+            return currentNumber;
+          }
+        }
+      }
+
+      return insideFunction;
+    }
+
+    var insideClosure = outsideClosure();
+    expect(insideClosure()).toBe(6);//The closure respects the final state of the variables
+  });
 
 });
