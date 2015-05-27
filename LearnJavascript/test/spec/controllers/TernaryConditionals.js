ea680730c8b4f90b5136b8781d1ddd8af7f19a01
MartinChavez <martin.chavez@live.com>
2015-05-26 19:41:11 -0700

    Added thruthy classes

--- LearnJavascript/test/spec/controllers/TernaryConditionals.js
+++ LearnJavascript/test/spec/controllers/TernaryConditionals.js
@@ -21,46 +21,7 @@ describe('Ternary Conditionals', function () {
     value = (true) ? true : false;
     expect(value).toBe(true);
   });
-
-  /*Falsy Values*/
-  //In Javascript,any value that is  'false',0,'undefined','NaN',"" or null is considered Falsy
-  it('you can evaluate to false any Falsy value', function () {
-    var value;
-    value = (false) ? true : false;
-    expect(value).toBe(false);
-    value = (0) ? true : false;
-    expect(value).toEqual(false);
-    value = (undefined) ? true : false;
-    expect(value).toEqual(false);
-    value = (NaN) ? true : false;
-    expect(value).toBe(false);
-    value = ("") ? true : false;
-    expect(value).toEqual(false);
-    value = (null) ? true : false;
-    expect(value).toEqual(false);
-  });
-
-  /*Truthy  Values*/
-  //In Javascript,any value that is NOT 'false',0,'undefined','NaN',"" or null is considered Truthy
-  it('you can evaluate to false any Falsy value', function () {
-    var value;
-    value = ("String") ? true : false;
-    expect(value).toBe(true);
-    value = (1) ? true : false;
-    expect(value).toEqual(true);
-    var definedVar = value ;
-    value = (definedVar) ? true : false;
-    expect(value).toEqual(true);
-    value = (-2) ? true : false;
-    expect(value).toEqual(true);
-    value = ([]) ? true : false;
-    expect(value).toEqual(true);
-    value = ({}) ? true : false;
-    expect(value).toEqual(true);
-    value = (Object.prototype) ? true : false;
-    expect(value).toEqual(true);
-  });
-
+  
   it('A Ternary operator can execute expressions', function () {
     var assignedVariable;
       //You can add several conditions before the ?
