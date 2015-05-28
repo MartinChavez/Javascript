93d5fbde7d7d80f7aea1b921e7e8f45bef60fc63
MartinChavez <martin.chavez@live.com>
2015-05-28 16:07:27 -0700

    Loop Optimization

--- LearnJavascript/test/spec/controllers/Loops.js
+++ LearnJavascript/test/spec/controllers/Loops.js
@@ -13,11 +13,37 @@ describe('Loops', function () {
     expect(currentNumber).toBe(5);
   });
   /*For-Loop*/
-  it('The for-loop runs has three sections and is adifferent way of producing looping behavior', function () {
+  it('The for-loop runs has three sections and it is a different way of producing looping behavior', function () {
     //   Initialization        Conditions        Statements that control the loop
     for (var currentNumber = 0;currentNumber <5 ;currentNumber++) {
       //All statements inside the body will be executed during each loop
     }
     expect(currentNumber).toBe(5);
+  });
+
+  /*Performance Optimizations*/
+  it('You can minimize memory access inside for loops', function () {
+    var numbers = [0,1,2,3,4,5,6,7,8,9];
+    //At the start of every cycle, the program needs to access the numbers array and access it's length property
+    for (var index = 0;index < numbers.length ;index++) {
+      expect(numbers[index]).toBe(index);
+    }
+    //You can use cached values to improve the loop performance by reducing memory access
+    //By creating both variables in the loop parameters,
+    //we signal to the compiler that they are only intended for use inside the loop
+    for (var index = 0,numbersLength = numbers.length;index < numbersLength ;index++) {
+      expect(numbers[index]).toBe(index);
+    }
+  });
+  it('You should use for-loops(vs in-loops) for array iteration', function () {
+    Array.prototype.extraMethod = function (){};
+
+    var numbers = [0,1,2];
+    //Using a property approach to access indices will also add in all the methods that have been added to the prototype
+    for (var property in numbers) {
+      if(isNaN(property)){
+        expect(property).toBe('extraMethod');
+      }
+    }
   });
 });
