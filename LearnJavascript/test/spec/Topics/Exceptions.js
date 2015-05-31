de8db37f8a175f6108d07839c4adf4dbb78ba3f0
MartinChavez <martin.chavez@live.com>
2015-05-31 15:27:30 -0700

    Renamed To Topics

---
+++ LearnJavascript/test/spec/Topics/Exceptions.js
@@ -0,0 +1,78 @@
+'use strict';
+
+describe('Exceptions', function () {
+
+  /*Exceptions : Run-time errors*/
+
+  it('Run-timer errors happen after JS is interpreted', function () {
+    //A syntax error will not make it past the interpreter. It will be marked as invalid Javascript
+    //It will be reported in the JS console
+    /*var brokenVar = ;*/
+    expect( function(){ return brokenVar; } ).toThrow();
+  });
+  it('A try block is a statement that allows the run-time to continue even when there are exceptions', function () {
+    var reachedCatch = false;
+    try{
+      brokenVar;
+    }catch(error){ //If the Try block throws an error, the information is passed to the catch block
+      expect(error.toString()).toBe("ReferenceError: Can't find variable: brokenVar");
+      reachedCatch = true;
+    }
+    expect(reachedCatch).toBe(true);
+  });
+  it('Error types allow us to take more informed actions', function () {
+    //Javascript can throw multiple useful error types that allow a catch block to handle errors more gracefully
+    try{
+      brokenVar;
+    }catch(error){
+      /*Reference Error is thrown when the variable has not been defined*/
+      expect(error instanceof ReferenceError).toBe(true)
+    }
+    try{
+      var brokenVar = 3;
+      brokenVar.concat("Three");
+    }catch(error){
+      /*TypeError is thrown when the variable is a different Type from what was expected
+      and does not have the desired method*/
+      expect(error instanceof TypeError).toBe(true)
+    }
+  });
+  it('You can use conditionals and throw keyword to control the exception flow', function () {
+    var reachedCatch = false;
+    try{
+      if(brokenVar === undefined){
+        throw new ReferenceError();
+      }
+    }catch(error){
+      expect(error instanceof ReferenceError).toBe(true);
+      reachedCatch = true;
+      //You can now add a more specific message to console about the reason if threw
+    }
+    expect(reachedCatch).toBe(true);
+  });
+
+  it('You can use the finally block after a catch block to execute code that should run even if there are errors on the try block', function () {
+    var reachedFinally = false;
+    try{
+      brokenVar;
+    }catch(error){
+    //The finally block will execute whether try met with success or failure
+    }finally{
+      reachedFinally = true;
+    }
+    expect(reachedFinally).toBe(true);
+  });
+  it('You can nest try-blocks within catch-blocks to organize the option sequence', function () {
+   var reachedLastCatchBlock = false;
+    try{
+      throw new Error();
+    }catch(error){
+      try{
+        throw new Error();
+      }catch(error2){
+        reachedLastCatchBlock = true;
+      }
+    }
+    expect(reachedLastCatchBlock).toBe(true);
+  });
+});
---
+++ LearnJavascript/test/spec/Topics/LogicalAssignment.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Logical Assignment', function () {
+
+  /*Logical Operators can make conditional assignments.*/
+
+  it('You can make assignments with ternary conditions', function () {
+    var object = {
+      addValueToArray : function(value){
+        //We can verify that this.objectArray previously exists (it is looking for a truthy), if that is the case, return the same value
+        //Otherwise, return a new array
+        this.objectArray = this.objectArray ? this.objectArray : [];
+        this.objectArray.push(value);
+      }
+    };
+    expect(object.objectArray).toBeUndefined();
+    object.addValueToArray(5);
+    expect(object.objectArray[0]).toBe(5);
+  });
+  it('You can make assignments with logical operators making the code less verbose', function () {
+    var object = {
+      addValueToArray : function(value){
+        //Using the || during assignment, it will return the first Truthy value
+        this.objectArray =  this.objectArray || [];
+        this.objectArray.push(value);
+      }
+    };
+    expect(object.objectArray).toBeUndefined();
+    object.addValueToArray(5);
+    expect(object.objectArray[0]).toBe(5);
+  });
+  it('Logical Operators results for ||', function () {
+    //The || assignment returns the first thruthy value
+    expect(4||5).toBe(4);
+    //If some values are truthy and others are falsey, the || assignment returns the first thruthy value
+    expect(4||0).toBe(4);
+    expect(0||4).toBe(4);
+    //If all  values  are falsey, the || assignment returns the last element
+    expect(0||""||undefined).toBeUndefined();
+  });
+
+  it('Logical Operators results for &&', function () {
+    //The && assignment returns the right-most thruthy value
+    expect(4&&5).toBe(5);
+    expect(5&&4).toBe(4);
+    //The && assignment returns the first falsey value
+    expect(4&&5&&0).toBe(0);
+    expect(5&&4&&undefined).toBeUndefined();
+    expect(""&&null&&0).toBe("");
+
+    //If all  values  are truthy, the && assignment returns the last element
+    expect(1&&2&&3).toBe(3);
+  });
+
+});
