a1490698f996a2f67f8a0c4bfd574551a3db73c2
MartinChavez <martin.chavez@live.com>
2015-05-31 15:16:31 -0700

    Deleting Bower dependencies

--- LearnJavascript/test/karma.conf.js
+++ LearnJavascript/test/karma.conf.js
@@ -18,16 +18,6 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
