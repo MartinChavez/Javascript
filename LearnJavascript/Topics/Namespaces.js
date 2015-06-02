f795726f19b776f1ffbd0243fffbb7ee030f4705
MartinChavez <martin.chavez@live.com>
2015-06-01 21:07:24 -0700

    Namespaces

--- LearnJavascript/Topics/Namespaces.js
+++ LearnJavascript/Topics/Namespaces.js
@@ -33,4 +33,30 @@ describe('Namespaces', function () {
     expect(NAMESPACE.INTERNAL.namespaceVariable).toBe(10);
   });
 
+  /*Anonymous Closures*/
+
+  //When you create NAMESPACES, the program may still execute its private methods and variables
+  //It is good practice to use private methods to safely modify private data
+  //Anonymous Closures allow you to define public and private variables
+  it('Closure will allow you to make private variables and properties', function () {
+    //You can achieve this by surrounding the entire set of properties and values in an immediately
+    //invoked function expression
+    //The local values and methods will be "closed" into the armory's namespace
+    var NAMESPACE = (function(){
+      var privateArray  = [1,2,3];
+      var privateVariable = 9;
+      //In order to make public properties, you can return an object
+      return {
+        //Since the function expression is invoked, this returned object will be handled immediately
+        // to the NAMESPACE variable and become a namespace
+        publicArray: function(){return [4,5,6];},
+        publicVariable: 10
+      };
+    })();//This parentheses indicate that the function expression should be immediately executed
+    //Since privateArray and privateVariable, we expect them to be undefined
+    expect(NAMESPACE.privateArray).toBeUndefined();
+    expect(NAMESPACE.privateVariable).toBeUndefined();
+    expect(NAMESPACE.publicArray()).toEqual([ 4, 5, 6 ]);
+    expect(NAMESPACE.publicVariable).toBe(10);
+  });
 });
