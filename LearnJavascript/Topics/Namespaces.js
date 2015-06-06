a2c338ac2e860a0999522b00dfe9a9e468c79b11
MartinChavez <martin.chavez@live.com>
2015-06-06 08:08:44 -0700

    Namescpaces extraction

--- LearnJavascript/Topics/Namespaces.js
+++ LearnJavascript/Topics/Namespaces.js
@@ -86,4 +86,31 @@ describe('Namespaces', function () {
     expect(NAMESPACE.globalVariable).toBe(7);
   });
 
+  /*Augmentation*/
+
+  //Provides extra properties for existing modules
+
+  it('You can add functionality to an existing namespace by Augmentation', function () {
+
+    var NAMESPACE = (function () {
+      var privateVariable = 5;
+      return {
+        publicVariable: 7
+      };
+    })();
+    //To create augmentation, we create another closure
+    NAMESPACE = (function(OLDNAMESPACE){
+      //It is very important to understand that only methods on this namespace can access the private values of this closure
+      var privateVariableTwo = 10;
+      OLDNAMESPACE.getPrivateVariableTwo = function (){
+        return privateVariableTwo;
+      };
+      return OLDNAMESPACE;
+    })(NAMESPACE);
+
+    expect(NAMESPACE.publicVariable).toBe(7);
+    expect(NAMESPACE.getPrivateVariableTwo()).toBe(10);
+
+  });
+
 });
